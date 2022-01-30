607a62214652fda324fd75fc8c55cf65e4db120e
Ksenia <kolmakova.ksyusha@mail.ru>
2022-01-30 22:44:20 +0300

    Fixed comments, added loading DB on docker compose

--- utils/db/employeeDB.sql
+++ utils/db/employeeDB.sql
@@ -1,18 +1,30 @@
-CREATE TABLE IF NOT EXISTS public.employee
-(
-    employee_id integer NOT NULL DEFAULT nextval('employee_employee_id_seq'::regclass),
-    first_name text COLLATE pg_catalog."default",
-    last_name text COLLATE pg_catalog."default",
+DROP DATABASE IF EXISTS employeedb;
+
+CREATE DATABASE employeedb WITH ENCODING = 'UTF8';
+\connect employeedb
+SET client_encoding = 'UTF8';
+
+CREATE TABLE public.employee (
+    employee_id integer NOT NULL,
+    first_name text,
+    last_name text,
     department_id integer,
-    job_title text COLLATE pg_catalog."default",
-    gender text COLLATE pg_catalog."default",
-    date_of_birth date,
-    CONSTRAINT employee_pk PRIMARY KEY (employee_id)
-)
+    job_title text,
+    gender text,
+    date_of_birth date
+);
 
-TABLESPACE pg_default;
+CREATE SEQUENCE public.employee_employee_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
-ALTER TABLE IF EXISTS public.employee OWNER to root;
+ALTER SEQUENCE public.employee_employee_id_seq OWNED BY public.employee.employee_id;
+ALTER TABLE ONLY public.employee ALTER COLUMN employee_id SET DEFAULT nextval('public.employee_employee_id_seq'::regclass);
+ALTER TABLE ONLY public.employee ADD CONSTRAINT employee_pk PRIMARY KEY (employee_id);
 
 INSERT INTO "public"."employee" (FIRST_NAME,LAST_NAME,DEPARTMENT_ID,JOB_TITLE,GENDER,DATE_OF_BIRTH) VALUES
      ('Elena','Kotova',1,'manager','FEMALE','1990-04-08'),
